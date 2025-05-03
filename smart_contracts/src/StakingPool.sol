52c900aa6b0022985b6756a10ae4b335434947c1
anotherdev <joseluismanco37@gmail.com>
2025-05-03 01:12:44 -0500

    fix: minimal issues

--- smart_contracts/src/StakingPool.sol
+++ smart_contracts/src/StakingPool.sol
@@ -1,34 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-import {INemoLiquifier} from "./interfaces/INemoLiquifier.sol";
+import {INeemoLiquifier} from "./interfaces/INeemoLiquifier.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StakingPool {
-    INemoLiquifier public immutable nemoLiquifier;
+    INeemoLiquifier public immutable neemoLiquifier;
     ERC20 public immutable ASTR;
     ERC20 public immutable NsASTR;
     mapping(address => uint256) public stakedAmount;
 
-    constructor(address _nemoLiquifier, address _astr, address _nsastr) {
-        nemoLiquifier = INemoLiquifier(_nemoLiquifier);
+    constructor(address _neemoLiquifier, address _astr, address _nsastr) {
+        neemoLiquifier = INeemoLiquifier(_neemoLiquifier);
         ASTR = ERC20(_astr);
         NsASTR = ERC20(_nsastr);
     }
 
     function stake(uint256 amount) public {
         ASTR.transferFrom(msg.sender, address(this), amount);
-        ASTR.approve(address(nemoLiquifier), amount);
-        nemoLiquifier.swapUnderlyingToLst(amount, address(this), address(0));
+        ASTR.approve(address(neemoLiquifier), amount);
+        neemoLiquifier.swapUnderlyingToLst(amount, address(this), address(0));
         stakedAmount[msg.sender] += amount;
     }
 
     function unstake(uint256 amount) public {
-        //TODO: Implement unstake
+        // neemoLiquifier.swapLstToUnderlying(amount, address(this), address(0));
+        // NsASTR.transfer(msg.sender, amount);
+        // stakedAmount[msg.sender] -= amount;
     }
 
     function withdraw(uint256 amount) public {
-        //TODO: Implement withdraw
+        // ASTR.transfer(msg.sender, amount);
     }
 
-    
+
 }
