a214f55323fcf27f9ea48f94d111c84d138d2e9e
anotherdev <joseluismanco37@gmail.com>
2025-05-03 23:04:54 -0500

    fix: fixes fixes

--- smart_contracts/src/PlatoCoin.sol
+++ smart_contracts/src/PlatoCoin.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-contract PlatoCoint is ERC20 {
+contract PlatoCoin is ERC20 {
     constructor() ERC20("PlatoCoin", "PC"){
     }
 
