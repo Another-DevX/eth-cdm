c0a0526dad03f6ba500e6caaaf4c932c72cbd9ec
cold-briu <39354014+cold-briu@users.noreply.github.com>
2025-05-04 04:50:04 -0500

    fix: env

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,7 +1,7 @@
 
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import {soneium} from '@reown/appkit/networks'
+import { soneium } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = '86e50dcaacf00969397544935c21968d'
@@ -10,11 +10,14 @@ if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
+// Get API key from environment variable
+const API_KEY = process.env.NEXT_PUBLIC_SCS_API_KEY
+
 export const soneium_scs = {
   ...soneium,
   rpcUrls: {
     default: {
-      http: ['https://soneium.rpc.scs.startale.com?apikey=TOsbOdqgCfLjFqxm6AUdpsRqo660y2Sv']
+      http: [`https://soneium.rpc.scs.startale.com?apikey=${API_KEY}`]
     }
   }
 }
